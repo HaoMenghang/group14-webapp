b0c7e2b47cdc5a89b5160a72aa6aab5ba9ac79f8
HaoMenghang <43610654+HaoMenghang@users.noreply.github.com>
2019-11-30 15:35:50 +0000

    1

---
+++ my-app/build/precache-manifest.95028dd2cf468ac64ffcac237a9ab892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adae3058bbba5c64ceb6cfa98589368",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c959f3130e338ffbe3",
+    "url": "/static/css/main.78400da4.chunk.css"
+  },
+  {
+    "revision": "b2110c3ce2387e3c8ff4",
+    "url": "/static/js/2.7dc8e66b.chunk.js"
+  },
+  {
+    "revision": "b1c959f3130e338ffbe3",
+    "url": "/static/js/main.8be8582c.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  }
+]);
